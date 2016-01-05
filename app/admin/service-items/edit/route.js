5aed061676abf5720459384e9cfd0d4b531d1097
Brandi Soucy <bbsoucy@gmail.com>
2016-01-05 15:58:54 -0600

    setting up detail edit and new routes

--- app/admin/service-items/edit/route.js
+++ app/admin/service-items/edit/route.js
@@ -1,4 +1,13 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  actions: {
+    saveService(formValues) {
+      let service = this.modelFor('service-items.edit');
+      service.setProperties(formValues);
+      service.save().then(() => {
+        this.transitionTo('admin.detail');
+      });
+    },
+  },
 });
