2333532c9a803a27314a83181ca36303891f65f2
Brandi Soucy <bbsoucy@gmail.com>
2016-01-05 09:31:06 -0600

    creating a client info model

---
+++ app/models/client-info.js
@@ -0,0 +1,12 @@
+import DS from 'ember-data';
+import Ember from 'ember';
+
+export default DS.Model.extend({
+  firstName: DS.attr('string'),
+  lastName: DS.attr('string'),
+  email: DS.attr('string'),
+  address: DS.attr('string'),
+
+  history: DS.hasMany('client-history'),
+
+});
