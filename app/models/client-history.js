dd8e9e18f67d21f4895c2bee3dc4fff711c9a19b
Brandi Soucy <bbsoucy@gmail.com>
2016-01-05 13:57:11 -0600

    setting up client-history model

--- app/models/client-history.js
+++ app/models/client-history.js
@@ -1,5 +1,11 @@
 import DS from 'ember-data';
+import Ember from 'ember';
 
 export default DS.Model.extend({
-  
+  date: DS.attr('date'),
+  service: DS.attr('string'),
+  price: DS.attr('number'),
+
+  history: DS.hasMany('client-history'),
+
 });
