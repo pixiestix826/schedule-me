616c6e151be4a065590419180c1e40f90f2cf58c
Brandi Soucy <bbsoucy@gmail.com>
2016-01-04 23:03:31 -0600

    working through console errors

--- app/new-user/route.js
+++ app/new-user/route.js
@@ -1,4 +1,18 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  statsTracker: Ember.inject.service('session'),
+
+  actions: {
+    addClient(formValues) {
+      let client = this.store.createRecord('client-info', formValues);
+
+      client.save().then(() => {
+        // Send info that we have created a new client createRecord
+        this.get('session').newClientWasAdded();
+
+        this.transitionTo('client.detail');
+      });
+    },
+  },
 });
