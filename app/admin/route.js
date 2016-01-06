d56dfcae467040c9446d033a5097f638d24cdf38
Brandi Soucy <bbsoucy@gmail.com>
2016-01-06 13:59:09 -0600

    adding authenticated routes

--- app/admin/route.js
+++ app/admin/route.js
@@ -1,19 +1,25 @@
 import Ember from 'ember';
+import AuthenticatedRouteMixin from 'ember-simple-auth/mixins/authenticated-route-mixin';
 
-export default Ember.Route.extend({
+let {
+  RSVP
+} = Ember;
+
+export default Ember.Route.extend(AuthenticatedRouteMixin, {
   currentUser: Ember.inject.service('current-user'),
 
   beforeModel() {
-   return new RSVP.Promise((resolve, reject) => {
-     return this.get('sessionUser.user').then((user) => {
-       if (Ember.get(user, 'isAdmin')) {
-         return resolve();
-       }
+    this._super(...arguments);
+    return new RSVP.Promise((resolve, reject) => {
+      return this.get('sessionUser.user').then((user) => {
+        if (Ember.get(user, 'isAdmin')) {
+          return resolve();
+        }
 
-       // Make the user go to the login screen
-       this.transitionTo('login');
-       return reject();
-     });
-   });
- },
+        // Make the user go to the login screen
+        this.transitionTo('login');
+        return reject();
+      });
+    });
+  },
 });
--- app/models/profile.js
+++ app/models/profile.js
@@ -1,7 +1,7 @@
 import DS from 'ember-data';
 import IAm from 'ember-i-am/mixins/i-am';
 
-export default DS.Model.extend({
+export default DS.Model.extend(IAm, {
   email: DS.attr(),
   firstName: DS.attr(),
   lastName: DS.attr(),
