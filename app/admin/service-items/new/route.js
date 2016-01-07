456f1040f507dc6f16f6536d0067ce58d6e3335b
Brandi Soucy <bbsoucy@gmail.com>
2016-01-07 13:50:29 -0600

    records of services being created

--- app/admin/service-items/new/route.js
+++ app/admin/service-items/new/route.js
@@ -4,9 +4,7 @@ export default Ember.Route.extend({
   actions: {
     addService(formValues) {
       let service = this.store.createRecord('service-item', formValues);
-
       service.save().then(() => {
-        this.get('service-item').pushObject('services-list');
         this.transitionTo('admin.detail');
       });
     },
