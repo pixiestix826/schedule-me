e110fcc2d4082c36ca6e204e29f15cf668eb6d6a
Brandi Soucy <bbsoucy@gmail.com>
2016-01-08 14:00:19 -0600

    totally broken

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -1,23 +1,23 @@
-import Ember from 'ember';
-
-export default Ember.Controller.extend({
-  occurrences: Ember.A(),
-
-  actions: {
-    calendarAddOccurrence: function(occurrence) {
-      this.get('occurrences').pushObject(Ember.Object.create({
-        title: occurrence.get('title'),
-        startsAt: occurrence.get('startsAt'),
-        endsAt: occurrence.get('endsAt'),
-      }));
-    },
-
-    calendarUpdateOccurrence: function(occurrence, properties) {
-      occurrence.setProperties(properties);
-    },
-
-    calendarRemoveOccurrence: function(occurrence) {
-      this.get('occurrences').removeObject(occurrence);
-    }
-  }
-});
+// import Ember from 'ember';
+//
+// export default Ember.Controller.extend({
+//   occurrences: Ember.A(),
+//
+//   actions: {
+//     calendarAddOccurrence: function(occurrence) {
+//       this.get('occurrences').pushObject(Ember.Object.create({
+//         title: occurrence.get('title'),
+//         startsAt: occurrence.get('startsAt'),
+//         endsAt: occurrence.get('endsAt'),
+//       }));
+//     },
+//
+//     calendarUpdateOccurrence: function(occurrence, properties) {
+//       occurrence.setProperties(properties);
+//     },
+//
+//     calendarRemoveOccurrence: function(occurrence) {
+//       this.get('occurrences').removeObject(occurrence);
+//     }
+//   }
+// });
