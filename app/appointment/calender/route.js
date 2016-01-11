8d1d4a853a5c4e0cd094bd145a360487cd1e8c2b
Brandi Soucy <bbsoucy@gmail.com>
2016-01-11 15:54:54 -0600

    client can select a service

--- app/appointment/calender/route.js
+++ app/appointment/calender/route.js
@@ -1,4 +1,10 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  model() {
+    return Ember.RSVP.hash({
+      services: this.store.findAll('service-item'),
+      appointments: this.store.findAll('appointment-item'),
+    });
+  },
 });
