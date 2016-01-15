18ff89c871b863000099e53efecc992d86ae0d54
Brandi Soucy <bbsoucy@gmail.com>
2016-01-15 12:31:03 -0600

    writing templates and routes for services

--- app/professionals/services/route.js
+++ app/professionals/services/route.js
@@ -1,4 +1,11 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  currentUser: Ember.inject.service('current-user'),
+
+  model() {
+    return Ember.RSVP.hash({
+      services: this.store.findAll('service-item'),
+    });
+  },
 });
