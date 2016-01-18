c6e85e30a1c26b7295a9c3cc2b11ba03f7037b35
Brandi Soucy <bbsoucy@gmail.com>
2016-01-18 11:25:06 -0600

    trying to get calendars to sync

--- app/professionals/services/booking/route.js
+++ app/professionals/services/booking/route.js
@@ -1,21 +1,56 @@
+// import Ember from 'ember';
+//
+// export default Ember.Route.extend({
+//   professionals: Ember.inject.service('professionals'),
+//   model() {
+//     return Ember.RSVP.hash({
+//       services: this.store.findAll('service-item'),
+//       appointments: this.store.findAll('appointment-item'),
+//     });
+//   },
+//
+//   actions: {
+//     createAppointment(formValues) {
+//       let appointment = this.store.createRecord('appointment-item', formValues);
+//
+//       appointment.save().then(() => {
+//         this.transitionTo('client.detail');
+//       });
+//     },
+//   },
+// });
+
 import Ember from 'ember';
+import moment from 'moment';
 
-export default Ember.Route.extend({
+export default Ember.Component.extend({
   professionals: Ember.inject.service('professionals'),
-  model() {
-    return Ember.RSVP.hash({
-      services: this.store.findAll('service-item'),
-      appointments: this.store.findAll('appointment-item'),
+
+  _appointments: [],
+
+  didReceiveAttrs() {
+    let appointments = this.get('appointments');
+
+    this.set('_appointments', appointments.toArray());
+    this.renderCalendar();
+  },
+
+  renderCalendar() {
+    let appointments = this.get('_appointments');
+
+    let createAppointment = appointments.map(function(appointment) {
+      let length = appointment.get('length') || appointment.get('service.time');
+
+      return {
+        id: appointment.get('id'),
+        startsAt: appointment.get('startTime'),
+        endsAt: moment(appointment.get('startTime')).add(length, 'minute'),
+        title: appointment.get('title') || appointment.get('service.service'),
+        model: appointment,
+      };
     });
-  },
 
-  actions: {
-    createAppointment(formValues) {
-      let appointment = this.store.createRecord('appointment-item', formValues);
+    console.log(createAppointment);
 
-      appointment.save().then(() => {
-        this.transitionTo('client.detail');
-      });
-    },
+    this.set('createAppointment', createAppointment);
   },
-});
