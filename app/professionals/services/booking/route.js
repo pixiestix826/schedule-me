ecdbb19fe6b2a77879bb285df8d9fc4580bd3063
Brandi Soucy <bbsoucy@gmail.com>
2016-01-18 12:00:40 -0600

    appointments are logging but not rendering on calendar

--- app/professionals/services/booking/route.js
+++ app/professionals/services/booking/route.js
@@ -1,32 +1,21 @@
-// import Ember from 'ember';
-//
-// export default Ember.Route.extend({
-//   professionals: Ember.inject.service('professionals'),
-//   model() {
-//     return Ember.RSVP.hash({
-//       services: this.store.findAll('service-item'),
-//       appointments: this.store.findAll('appointment-item'),
-//     });
-//   },
-//
-//   actions: {
-//     createAppointment(formValues) {
-//       let appointment = this.store.createRecord('appointment-item', formValues);
-//
-//       appointment.save().then(() => {
-//         this.transitionTo('client.detail');
-//       });
-//     },
-//   },
-// });
 
 import Ember from 'ember';
 import moment from 'moment';
 
-export default Ember.Component.extend({
+export default Ember.Route.extend({
   professionals: Ember.inject.service('professionals'),
 
   _appointments: [],
+
+  actions: {
+    createAppointment(formValues) {
+      let appointment = this.store.createRecord('appointment-item', formValues);
+
+      appointment.save().then(() => {
+        this.transitionTo('client.detail');
+      });
+    },
+  },
 
   didReceiveAttrs() {
     let appointments = this.get('appointments');
