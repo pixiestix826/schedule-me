07dc6816b7befd12f1ff044a7e49ef6c5213c66b
Brandi Soucy <bbsoucy@gmail.com>
2016-01-20 11:09:31 -0600

    adding professional image to service page

--- app/professionals/services/index/route.js
+++ app/professionals/services/index/route.js
@@ -1,12 +1,12 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-  professionals: Ember.inject.service('professionals'),
-
   model() {
+    let professional = this.modelFor('professionals.services');
+
     return Ember.RSVP.hash({
       services: this.store.findAll('service-item'),
-      // professional: this.store.findRecord('professional'),
+      professional,
     });
   },
 
